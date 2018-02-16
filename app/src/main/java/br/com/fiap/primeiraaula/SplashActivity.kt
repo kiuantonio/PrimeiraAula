5625af9e2ff2317d041f8d485dbdbd1d6aaddc41
logonrm <logonrm@fiap.com.br>
2018-02-16 09:45:22 -0200

    antonio

---
+++ app/src/main/java/br/com/fiap/primeiraaula/SplashActivity.kt
@@ -0,0 +1,24 @@
+package br.com.fiap.primeiraaula
+
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+
+class SplashActivity : AppCompatActivity() {
+
+    val TEMPO_SPLASH = 3500L
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+        carregar()
+    }
+
+    fun carregar{
+        Handler().postDelayed({
+            startActivity(Intent(this,MainActivity::class.java))
+            finish()
+        },TEMPO_SPLASH)
+    }
+}
